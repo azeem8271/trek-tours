900f927a961350aeab1a7e799d0fe8e827598333
azeem8271 <azeemkhan07082000@gmail.com>
2024-02-05 16:10:08 +0530

    Chore: add import scripts for Tours, Users and Reviews

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const Tour = require('../../models/tourModel');
+const User = require('../../models/userModel');
+const Review = require('../../models/reviewModel');
 
 dotenv.config({ path: './config.env' });
 
@@ -19,14 +21,22 @@ const DB = process.env.DATABASE.replace(
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/tours.json`, { encoding: 'utf-8' }),
 );
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/users.json`, { encoding: 'utf-8' }),
+);
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, { encoding: 'utf-8' }),
+);
 
 // Add data to database
 const importData = async () => {
   try {
     await mongoose.connect(DB);
     console.log('Connected to Natours DB');
-    await Tour.insertMany(tours);
-    console.log('data Successfully added to Natours Tours');
+    await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
+    console.log('Tours, Users & Reviews data Successfully added to Natours...');
   } catch (error) {
     console.log(error);
   } finally {
@@ -40,7 +50,9 @@ const deleteData = async () => {
     await mongoose.connect(DB);
     console.log('Connected to Natours DB');
     await Tour.deleteMany();
-    console.log('data deleted Successfully');
+    await User.deleteMany();
+    await Review.deleteMany();
+    console.log('Tours, Users & Reviews deleted Successfully...');
   } catch (error) {
     console.log(error);
   } finally {
