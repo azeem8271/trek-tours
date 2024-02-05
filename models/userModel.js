530c84e34087f81d90ca4212cd6e24f7c12bba74
azeem8271 <azeemkhan07082000@gmail.com>
2024-02-05 16:13:43 +0530

    Refactor: update bcrypt salt value

--- models/userModel.js
+++ models/userModel.js
@@ -67,7 +67,7 @@ userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) {
     return next();
   }
-  this.password = await bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 12);
   this.passwordConfirm = undefined;
   next();
 });
